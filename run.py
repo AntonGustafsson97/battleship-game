14647d6fcb3f91a42b5d3a0bd964c03dbb17955f
Anton Gustafsson <anton-slhc@hotmail.com>
2024-01-30 15:38:27 +0000

    Add SCOPE, CREDS, SCOPED_CREDS, GSPREAD_CLIENT and SHEET variables

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Global variables assigned to allow access through Google APIs to gspread.
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+
+CREDS = credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('battleship-game')
